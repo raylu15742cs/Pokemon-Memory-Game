3c7a419fd59b5c1501ee3c5200abd600fef4bd03
raylu15742cs <raylu15742cs@gmail.com>
2022-08-31 10:12:40 -0700

    add footer file

---
+++ memory-card/src/Component/Footer.js
@@ -0,0 +1,13 @@
+import React , {Component} from "react";
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h3> Footer</h3>
+            </div>
+        )
+    }
+}
+
+export default Footer;
