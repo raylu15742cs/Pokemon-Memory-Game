600dbcd8a83431f8e8e746793c1f8c2037d07b08
raylu15742cs <raylu15742cs@gmail.com>
2022-08-31 10:01:22 -0700

    initial header file

--- memory-card/src/Component/Header.js
+++ memory-card/src/Component/Header.js
@@ -1,1 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+                <p>hi</p>
+            </header>
+            </div>
+        );
+    }
+}
+
+export default App;
