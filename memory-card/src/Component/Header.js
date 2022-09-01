7050b2b98cdbd4f4e455c81795e0c0c8051120a0
raylu15742cs <raylu15742cs@gmail.com>
2022-08-31 21:37:56 -0700

    build header

--- memory-card/src/Component/Header.js
+++ memory-card/src/Component/Header.js
@@ -6,7 +6,14 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-                <p> Copyright &copy; 2022 raylu15742</p>
+                <div className="leftheader">
+                    <h1>Pokemon Memory Game</h1>
+                    <p>Get points by clicking on an image but don't click on any more than once!</p>
+                </div>
+                <div className="rightHeader">
+                    <p>Score : 0</p>
+                    <p>Best Score : 0</p>
+                </div>
             </div>
         );
     }
