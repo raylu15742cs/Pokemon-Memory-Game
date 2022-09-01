339950788a571824c23e95f4ba0dfd25108ce77c
raylu15742cs <raylu15742cs@gmail.com>
2022-09-01 09:11:39 -0700

    add initial 12 cards

--- memory-card/src/Component/Main.js
+++ memory-card/src/Component/Main.js
@@ -3,10 +3,21 @@ import React , {Component} from "react";
 class Main extends Component {
     render() {
         return (
-            <div className="main">
-                <h1>Main</h1>
-            </div>
-        )
+          <div className="main">
+            <div className="cardOne"> one </div>
+            <div className="cardOne"> two</div>
+            <div className="cardOne"> three</div>
+            <div className="cardOne"> four</div>
+            <div className="cardOne">five</div>
+            <div className="cardOne">6</div>
+            <div className="cardOne">seven</div>
+            <div className="cardOne">eight</div>
+            <div className="cardOne">nine</div>
+            <div className="cardOne">ten</div>
+            <div className="cardOne">11</div>
+            <div className="cardOne">12</div>
+          </div>
+        );
     }
 }
 
