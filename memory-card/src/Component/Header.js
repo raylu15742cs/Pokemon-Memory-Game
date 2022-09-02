8015e9eda9c8b7e86229175dba160d8198bdc224
raylu15742cs <raylucs15742@gmail.com>
2022-09-02 09:06:27 -0700

    convert header into functional component

--- memory-card/src/Component/Header.js
+++ memory-card/src/Component/Header.js
@@ -1,21 +1,23 @@
-import React, {Component} from "react";
+import React from "react";
 
 // this will file will handle header
 
-class Header extends Component {
-    render() {
-        return (
-            <div className="header">
-                <div className="leftheader">
-                    <h1>Pokemon Memory Game</h1>
-                    <p>Get points by clicking on an image but don't click on any more than once!</p>
-                </div>
-                <div className="rightHeader">
-                    <h1>Score : 0 Best Score : 0</h1>
-                </div>
-            </div>
-        );
-    }
+
+function Header () {
+     return (
+       <div className="header">
+         <div className="leftheader">
+           <h1>Pokemon Memory Game</h1>
+           <p>
+             Get points by clicking on an image but don't click on any more than
+             once!
+           </p>
+         </div>
+         <div className="rightHeader">
+           <h1>Score : 0 Best Score : 0</h1>
+         </div>
+       </div>
+     )
 }
 
 export default Header;
