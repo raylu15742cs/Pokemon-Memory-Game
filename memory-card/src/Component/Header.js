6a2ca76203e305cfc5e94f0c9574ec4ae52eea8a
raylu15742cs <raylucs15742@gmail.com>
2023-03-06 13:41:42 -0800

    remove unnecessary and fix sizing

--- memory-card/src/Component/Header.js
+++ memory-card/src/Component/Header.js
@@ -6,10 +6,6 @@ function Header () {
      return (
        <div className="header">
            <h1>Pokemon Memory Game</h1>
-           <p>
-             Get points by clicking on an image but don't click on any more than
-             once!
-           </p>
        </div>
      )
 }
